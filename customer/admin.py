d3c219820db47634b575bbc2bee65d8c93187142
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-06-08 03:40:26 +0300

    add cloudinary setup

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,7 @@
+from customer.models import Neighborhood, Profile
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
+
