63664e0eee22901d8858baa565d359eec33b1ca8
Benjamin Obafemi <benjaminoba234@gmail.com>
2021-06-08 06:41:49 +0300

    test save profile

--- customer/tests.py
+++ customer/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Neighborhood, Profile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+  def setUp(self):
+      self.user = User(username='Benjamin', email='ben@gmail.com', password='Bananna')
+      self.neighborhood = Neighborhood(name='Sandalwood', location='karen', occupants_count=10)
+      self.user.save()
+      self.neighborhood.save()
+      self.profile = Profile(photo='http:cloudinary/devward/profile/ben.jpg', bio='I am amazing', user=self.user, neighborhood=self.neighborhood)
+      
+      # Testing  instance
+  def test_instance(self):
+      self.assertTrue(isinstance(self.profile,Profile))  
+  def test_save_method(self):
+      self.profile.save_profile()     
+      profile = Profile.objects.all()
+      self.assertTrue(len(profile) > 0) 
+
+
+
